f1d121ddc9ff1ebeb3a259452e87ca22fc014da9
ZeyadWaledEbrahim <zeyadwaled165@gmail.com>
2025-04-13 21:01:55 +0200

    Build Validation Schema

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, FormEvent, useState } from "react";
 import { formInputsList, productList } from "./components/data";
 import Modal from "./components/Modal";
 import ProductCard from "./components/ProductCard";
 import Button from "./components/ui/Button";
 import Input from "./components/Input";
 import { IProduct } from "./components/interfaces";
+import { productValidation } from "./components/validation";
 
 const App = () => {
-  // state
-  const [isOpen, setIsOpen] = useState(false)
-  const [product, setProduct] = useState<IProduct>({
-    id: "",
+  const defaultProduct = {
     title: "",
     description: "",
     src: "",
@@ -20,7 +18,10 @@ const App = () => {
       name: "",
       src: "",
     },
-  })
+  }
+  // state
+  const [isOpen, setIsOpen] = useState(false)
+  const [product, setProduct] = useState<IProduct>(defaultProduct)
   const OnChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
     setProduct((prevProduct) => ({
@@ -37,11 +38,21 @@ const App = () => {
   function close() {
     setIsOpen(false)
   }
+  function submitHandler(event: FormEvent<HTMLFormElement>): void {
+    event.preventDefault();
+    const errors = productValidation({title: product.title, description: product.description, src: product.src, price: product.price});
+    console.log(errors);
+  }
+
+  function closeHandler() {
+    setProduct(defaultProduct)
+    close();
+  }
 
   // Render
   const renderProductList = productList.map(product => <ProductCard key={product.id} product={product}/>);
   const renderFormInputsList = formInputsList.map(input => (
-    <div className="flex flex-col space-y-3">
+    <div className="flex flex-col space-y-3" key={input.id}>
       <label htmlFor={input.id} className="mb-[2px] text-sm font-medium text-gray-700" >{input.label}</label>
       <Input type="text" id={input.id} name={input.name} value={product[input.name]} onChange={OnChangeHandler} />
     </div>
@@ -54,13 +65,13 @@ const App = () => {
         {renderProductList}
       </div>
       <Modal isOpen={isOpen} onClose={close} title="Add A NEW PRODUCT">
-        <div className="space-y-3">
+        <form className="space-y-3" onSubmit={submitHandler}>
           {renderFormInputsList}
           <div className="flex items-center space-x-3">
-            <Button onClick={close} className="bg-indigo-700 hover:bg-indigo-800" width="w-full">Submit</Button>
-            <Button onClick={close} className="bg-gray-400 hover:bg-gray-500" width="w-full">Cancel</Button>
+            <Button className="bg-indigo-700 hover:bg-indigo-800" width="w-full">Submit</Button>
+            <Button onClick={closeHandler} className="bg-gray-400 hover:bg-gray-500" width="w-full">Cancel</Button>
           </div>
-        </div>
+        </form>
       </Modal>
     </main>
   );
---
+++ src/components/validation/index.tsx
@@ -0,0 +1,42 @@
+/**
+ * 
+ * @param product - Product object containing title, description, src, and price.
+ * @param product.title - Title of the product (string).
+ * @param product.description - Description of the product (string).
+ * @param product.src - Image URL of the product (string).
+ * @param product.price - Price of the product (string).
+ * @returns 
+ */
+
+export const productValidation = (product: {
+  title: string;
+  description: string;
+  src: string;
+  price: string;
+}) => {
+
+  const errors: { title: string; description: string; src: string; price: string; } =  {
+    title: "",
+    description: "",
+    src: "",
+    price: "",
+  }
+
+  const validUrl = /^(ftp|http|https):\/\/[^."]+$/.test(product.src);
+
+  if (!product.title.trim() || product.title.length < 10 || product.title.length > 80) {
+    errors.title = "Title must be between 10 and 80 characters long.";
+  } 
+  if (!product.description.trim() || product.description.length < 20 || product.description.length > 900) {
+    errors.description = "Description must be between 20 and 900 characters long.";
+  }
+  if (!product.src.trim() || !validUrl) {
+    errors.src = "Valid Image URL is invalid.";
+  }
+  if (!product.price.trim() || isNaN(Number(product.price)) || Number(product.price) <= 0) {
+    errors.price = "Valid Price is required.";
+  }
+
+
+  return errors;
+}
